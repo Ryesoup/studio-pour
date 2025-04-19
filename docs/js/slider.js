5a4b7bff5b1a72a89b41edee3fc41d8b4f00185f
최원석 <suop@choewonseog-ui-MacBookPro.local>
2025-04-20 05:14:34 +0900

    git add .

---
+++ docs/js/slider.js
@@ -0,0 +1,7 @@
+// 예시 데이터
+const participantCount = 150;
+const stockCount = 20;
+
+// DOM 업데이트
+document.getElementById('participantCount').textContent = participantCount;
+document.getElementById('stockCount').textContent = stockCount;
